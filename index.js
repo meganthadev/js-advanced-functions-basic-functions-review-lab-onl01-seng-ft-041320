5d917b65a4c76d8b2de5531138b4c38f4af1830c
Megan Horsfield <meganthadev@gmail.com>
2020-08-05 04:31:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-function saturdayFun(activity='roller-skate') {
-  console.log(`This Saturday, I want to ${activity}!`);
+function saturdayFun(activity="roller-skate") {
+  return(`This Saturday, I want to ${activity}!`);
 }
